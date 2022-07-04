e0dca2dc0ab09fcf6b10ac5b09928376ed9e2204
Luiz Felipe do Divino <lf.divino@gmail.com>
2022-07-04 20:09:40 -0300

    [FIX] Override Polling Heartbeat function

--- pos_multi_session/static/src/js/crosstab_bus.js
+++ pos_multi_session/static/src/js/crosstab_bus.js
@@ -6,19 +6,7 @@ odoo.define('pos_multi_session.CrossTab', function (require) {
 
     CrossTabBus.include({
         _heartbeat: function () {
-            let self = this;
-            let heartbeatValue = parseInt(this._callLocalStorage('getItem', 'heartbeat', 0));
-
-            if (this._isMasterTab) {
-                if (heartbeatValue !== this.lastHeartbeat) {
-                    console.log(
-                        "MS",
-                        self.pos_longpolling.pos.config.name,
-                        "heartbeatValue (" + heartbeatValue + ") != this.lastHeartbeat (" + this.lastHeartbeat + ")"
-                    );
-                }
-            }
-            this._super();
+            return;
         },
     });
 });
